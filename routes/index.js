56121c9be33f7366d2a7a4107926dde237361d96
zubair <subair_a2005@hotmail.com>
2023-12-16 17:38:37 +0400

    landing page api

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ import express from 'express'
 import testRoutes from '../api/test/index.js';
 const routes = express.Router();
 
-routes.use('/test',testRoutes);
+routes.use('/', async (req, res) => {
+   
+    res.send('Welcome to test api....');
+
+});
+
+routes.use('/test', testRoutes);
 
 export default routes;  
